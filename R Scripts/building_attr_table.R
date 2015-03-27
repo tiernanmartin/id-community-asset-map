30fff1e49ce97600ad1951a2bbc59a0b2e97f2c3
Tiernan <tiernan@uw.edu>
2015-03-27 15:54:49 -0700

    Update
    - geocoded KC apt building data and KC commercial building data
    - saved raw geocode output to ‘CommBldg_geocoded.csv’
    - haven’t yet executed code to save the raw apt. building data

--- R Scripts/building_attr_table.R
+++ R Scripts/building_attr_table.R
@@ -1,4 +1,11 @@
-### Load project's packages, install them if necessary
+#######################################################
+### CREATING THE ASSET MAP BUILDING ATTRIBUTE TABLE ###                                    ########
+#######################################################
+
+
+#########################################################
+### A) Install and load required packages #########
+#########################################################
 
 if (!require("gplots")) {
   install.packages("gplots", dependencies = TRUE)
@@ -10,61 +17,110 @@ if (!require("dplyr")) {
   library(dplyr)
 }
 
+if (!require("ggmap")) {
+  install.packages("ggmap", dependencies = TRUE)
+  library(ggmap)
+}
 
-### Load the data
+###############################################################
+### B) Read in data and transform it into database format ##
+###############################################################
 
-URM.df <- read.csv("ID_URM bldg inventory-5.csv")
+# Data used to create this shapefile attribute table is
+# hosted at https://github.com/tiernanmartin/id-community-asset-map/tree/master/data
 
-URM.df <- data.frame(URM.df, stringsAsFactors = FALSE)
+setwd("/Users/tiernanmartin/Documents/GitHub/id-community-asset-map//data") # Set the working directory
 
-### Add variable with 3-digit address
+list.files(path = ".") #Check out the names of the files
 
-URM.df <-
-  URM.df %>%
-  mutate(STNUM_short = substr((as.character(ST_NUM)),1,3), 
-         ADDRESS_edit = paste(STNUM_short,ST_NAME, sep = ' ')) 
+commbldg.df <- read.csv(file = "EXTR_CommBldg.csv", header = TRUE, sep = ",") # Bring in the .csv's as data frames
+resbldg.df <- read.csv(file = "EXTR_ResBldg.csv", header = TRUE, sep = ",")
+aptbldg.df <- read.csv(file = "EXTR_AptComplex.csv", header = TRUE, sep = ",")
+bldgpermits.df <- read.csv(file = "Building_Permits___2014-08-27.csv", header = TRUE, sep = ",")
 
-### Check the format of the columns (as required by Smarty Streets)
+##################################################################
+### C) Trim dataframes to only 98104, 98134, and 98144 records ###
+##################################################################
 
-address_example <- read.csv("example-lists/standard-address-input.csv")
+commbldg.df <- filter(commbldg.df, ZipCode == "98104" |
+                        ZipCode == "98134" |
+                        ZipCode == "98144")
 
-### Create new .csv to paste into Smarty Streets
+resbldg.df <- filter(resbldg.df, ZipCode == "98104" |
+                        ZipCode == "98134" |
+                        ZipCode == "98144")
 
-names(URM.df)
-
-smarty.df <-
-  URM.df %>%
-  select(Address = ADDRESS_edit, City = CITY, State = STATE) #Select the rows and rename them to match Smarty's reqs
-
-
-### Load Smarty file
-
-write.csv(smarty.df, file="smarty.csv")
+aptbldg.df <- aptbldg.df[grepl("98104$|98134$|98144$", aptbldg.df$Address), ] #Uses regex to filter by zip code because the zip code is embedded in the full address
 
 
-### This will add the data frame to the clipboard
+###############################################################################
+### D) Geocode the addresses then trim dataframes by Latitude and Longitude ###
+###############################################################################
 
-clip <- pipe("pbcopy", "w") 
-write.table(smarty.df, file=clip, col.names = TRUE)
-close(clip) # ... that didn't work... I had to open the .csv in excel and copy + paste into the browser
+latupper <- 47.601171   # Sets variables for the latitude and longitude boundary 
+latlower <- 47.593863
+longwest <- -122.328946
+longeast <- -122.310483
 
-rm(clip)
+aptbldg.df$Address <- as.character(aptbldg.df$Address) # Change Address column from factor to character
 
-smarty_zips.df <- read.csv("smarty_full.csv")
-names(smarty_zips.df)
+#NOTE:  the following process takes about 10 minutes. Remove the '#' in order to run it.
+#aptbldg_geocoded.df <- geocode(aptbldg.df$Address, output = "latlona") #Geocode the addresses (uses Google Maps API)
 
-### Trim down the table and write a new one
+aptbldg.df <-      # Create a new dataframe with Lat., Long., and any other desired columns
+  aptbldg.df %>%
+   mutate(Address = aptbldg_geocoded.df$address,  # Add geocoded Address, Long., Lat.
+         Longitude = aptbldg_geocoded.df$lon,
+         Latitude = aptbldg_geocoded.df$lat) %>%
+   select(Major,                                  # Select desired columns
+         Minor,
+         Address,
+         Longitude,
+         Latitude,
+         Description = ComplexDescr,
+         Stories = NbrStories,
+         NbrBldgs,
+         Units = NbrUnits,
+         AvgUnitSize,
+         Quality = BldgQuality,
+         Condition,
+         Year = YrBuilt) %>%
+   filter(Latitude <= latupper &                  # Filter by Long., Lat.
+             Latitude >= latlower &
+             Longitude >= longwest &
+             Longitude <= longeast) %>%
+   arrange(desc(Latitude))                        # Arrange by Lat. (descending)
 
-names(URM.df)
+commbldg.df$Address <- as.character (commbldg.df$Address)
+#commbldg_geocoded.df <- geocode(commbldg.df$Address, output = "latlona")
 
-smarty_trim.df <- 
-  smarty_zips.df %>%
-  select(ADDRESS_edit = Address, Zipcode = X.zipcode., Latitude = X.latitude., Longitude = X.longitude.)
+commbldg.df <-
+    commbldg.df %>%
+    mutate(Address = commbldg_geocoded.df$address,
+           Longitude = commbldg_geocoded.df$lon,
+           Latitude = commbldg_geocoded.df$lat) %>%
+    select(Major,
+           Minor,
+           Address,
+           Longitude,
+           Latitude,
+           Description = BldgDescr,
+           Stories = NbrStories,
+           NbrBldgs,
+           Quality = BldgQuality,
+           Year = YrBuilt) %>%
+   filter(Latitude <= latupper &          
+           Latitude >= latlower &
+           Longitude >= longwest &
+           Longitude <= longeast) %>%
+   arrange(desc(Latitude))
 
-URM_join <- left_join(URM.df, smarty_trim.df) # joins using the 'ADDRESS_edit' variable
 
-names(URM_join)
+bldgpermits.df <- filter(bldgpermits.df,               
+                         Latitude <= latupper &
+                           Latitude >= latlower &
+                           Longitude >= longwest &
+                           Longitude <= longeast)
 
-write.csv(URM_join, file = "URM_join.csv")
 
 
